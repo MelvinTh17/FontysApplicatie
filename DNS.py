acbae3e500ee1148f8519512452a14f5d4a355d3
Melvinth17 <melvin.thijssen@gmail.com>
2025-02-19 09:49:46 +0100

    begin nieuw project

---
+++ DNS.py
@@ -0,0 +1,87 @@
+import dns.resolver
+
+
+def check_domein(domein):
+
+    records = [
+        'NONE',
+        'A',
+        'NS',
+        'MD',
+        'MF',
+        'CNAME',
+        'SOA',
+        'MB',
+        'MG',
+        'MR',
+        'NULL',
+        'WKS',
+        'PTR',
+        'HINFO',
+        'MINFO',
+        'MX',
+        'TXT',
+        'RP',
+        'AFSDB',
+        'X25',
+        'ISDN',
+        'RT',
+        'NSAP',
+        'NSAP-PTR',
+        'SIG',
+        'KEY',
+        'PX',
+        'GPOS',
+        'AAAA',
+        'LOC',
+        'NXT',
+        'SRV',
+        'NAPTR',
+        'KX',
+        'CERT',
+        'A6',
+        'DNAME',
+        'OPT',
+        'APL',
+        'DS',
+        'SSHFP',
+        'IPSECKEY',
+        'RRSIG',
+        'NSEC',
+        'DNSKEY',
+        'DHCID',
+        'NSEC3',
+        'NSEC3PARAM',
+        'TLSA',
+        'HIP',
+        'CDS',
+        'CDNSKEY',
+        'CSYNC',
+        'SPF',
+        'UNSPEC',
+        'EUI48',
+        'EUI64',
+        'TKEY',
+        'TSIG',
+        'IXFR',
+        'AXFR',
+        'MAILB',
+        'MAILA',
+        'ANY',
+        'URI',
+        'CAA',
+        'TA',
+        'DLV',
+    ]
+    
+    for rec in records:
+        try:
+            antwoord = dns.resolver.resolve(domein, rec)
+            for rdata in antwoord:
+                print(rec, ':', rdata.to_text())
+        except:
+            quit
+    
+
+if __name__ == '__main__':
+    check_domein('fontys.nl')
